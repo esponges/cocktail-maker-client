0513d0aaba59a9bc42b2a2be1a950840b60d1b93
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-07 09:24:40 -0600

    imp ws

--- src/components/context/dep-provider.tsx
+++ src/components/context/dep-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useRef } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import Dexie, { type EntityTable } from "dexie";
 
 import { Toaster } from "../ui/toaster";
@@ -32,7 +32,7 @@ export const DepContext = createContext<{
   };
   refs?: {
     form: React.RefObject<HTMLFormElement>;
-  }
+  };
   // more dependency clients can be added here
 }>({});
 
@@ -40,10 +40,6 @@ export function DepProvider({ children }: { children: React.ReactNode }) {
   const idxdb = new Dexie("cocktail_maker") as Dexie & {
     cocktails: EntityTable<Cocktail, "id">;
   };
-  const refs = {
-    form: useRef<HTMLFormElement>(null),
-  };
-
 
   idxdb.version(1).stores({
     cocktails:
@@ -51,10 +47,46 @@ export function DepProvider({ children }: { children: React.ReactNode }) {
       "++id, name, description, steps, is_alcoholic, mixers, size, cost, complexity, required_ingredients, required_tools, base_ingredients",
   });
 
+  // refs for navigation
+  const refs = {
+    form: useRef<HTMLFormElement>(null),
+  };
+
+  // using a websocket to spin up the server until we get a persistent server
+  const [ws, setWebsocket] = useState(null as WebSocket | null);
+  const join = (uuid: string) => {
+    const URL = `${process.env.NEXT_PUBLIC_WS_URL}/${uuid}`;
+
+    setWebsocket(() => {
+      const webSocket = new WebSocket(URL);
+
+      webSocket.onmessage = (_e) => {
+        // console.log(e.data);
+      };
+      webSocket.onclose = () => {
+        console.log("Connection Closed");
+      };
+
+      return webSocket;
+    });
+  };
+
+  useEffect(() => {
+    if (!ws) {
+      join(crypto.randomUUID());
+    }
+
+    return () => {
+      ws?.close();
+    };
+  }, [ws]);
+
   return (
     <>
       <Toaster />
-      <DepContext.Provider value={{ idxdb, refs }}>{children}</DepContext.Provider>
+      <DepContext.Provider value={{ idxdb, refs }}>
+        {children}
+      </DepContext.Provider>
     </>
   );
 }
