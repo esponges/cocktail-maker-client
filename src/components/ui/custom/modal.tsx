6058ae2e7e8b64f5b2ab65c7b48ff8a829dc8ab3
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-14 10:16:57 -0600

    outside click

--- src/components/ui/custom/modal.tsx
+++ src/components/ui/custom/modal.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/alert-dialog";
 
 import { type AlertDialogActionProps } from "@radix-ui/react-alert-dialog";
+import { useCallback, useEffect, useRef } from "react";
 
 type Props = {
   isOpen: boolean;
@@ -35,23 +36,54 @@ export function Modal({
   onCancel,
   continueBtnProps,
 }: Props) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const checkOutsideClick = useCallback(
+    (e: MouseEvent) => {
+      const HTMLTarget = e.target as HTMLElement;
+
+      // hack - use a wrapper only class to check if the user clicked outside the modal
+      if (HTMLTarget.classList.contains("bg-black/80")) {
+        onClose?.();
+      }
+    },
+    [onClose]
+  );
+
+  // close modal on outside click
+  useEffect(() => {
+    if (isOpen) {
+      document.body.addEventListener("click", checkOutsideClick);
+    }
+
+    return () => {
+      if (!isOpen) return;
+      document.body.removeEventListener("click", checkOutsideClick);
+    };
+  }, [onClose, isOpen, checkOutsideClick]);
+
   return (
     <AlertDialog
       open={isOpen}
       onOpenChange={onClose}
     >
-      <AlertDialogContent className={className}>
+      <AlertDialogContent
+        className={className}
+        ref={ref}
+      >
         <AlertDialogHeader>
           {title ? <AlertDialogTitle>{title}</AlertDialogTitle> : null}
           <AlertDialogDescription>{children}</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          {onCancel ? <AlertDialogCancel onClick={onCancel}>Cancel</AlertDialogCancel> : null}
+          {onCancel ? (
+            <AlertDialogCancel onClick={onCancel}>Cancel</AlertDialogCancel>
+          ) : null}
           <AlertDialogAction
             onClick={onSubmit}
             {...continueBtnProps?.props}
           >
-            {continueBtnProps?.label || 'Ok'}
+            {continueBtnProps?.label || "Ok"}
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
