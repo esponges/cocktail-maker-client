7816a8f600da7808f1a0798570f001e0a27f442b
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-16 17:08:09 -0600

    display prev cocktails

--- src/app/account/cocktails/page.tsx
+++ src/app/account/cocktails/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { useContext, useEffect, useState } from "react";
-import { Cocktail, DepContext } from "@/components/context/dep-provider";
+
+import { type Cocktail, DepContext } from "@/components/context/dep-provider";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { CocktailCard } from "@/components/ui/custom/cocktail-card";
 
 export default function AccountCocktails() {
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
-  const [selectedCocktail, setSelectedCocktail] = useState<Cocktail>();
   const { idxdb } = useContext(DepContext);
 
   // probably will want to make this a hook instead
@@ -14,12 +21,27 @@ export default function AccountCocktails() {
 
     idxdb.cocktails.toCollection().toArray().then(setCocktails);
   }, [idxdb]);
-  
-  console.log(cocktails);
 
   return (
     <main>
       <h1>Account Cocktails</h1>
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full"
+      >
+        {cocktails.map((cocktail) => (
+          <AccordionItem
+            key={cocktail.id}
+            value={cocktail.id}
+          >
+            <AccordionTrigger>{cocktail.name}</AccordionTrigger>
+            <AccordionContent>
+              <CocktailCard cocktail={cocktail} />
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </main>
   );
 }
